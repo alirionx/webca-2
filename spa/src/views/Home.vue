1f5dd5c0763ecc14d30965abd72bb6a5fd39448a
alirionx <dquilitzsch@outlook.de>
2021-06-25 12:11:54 +0200

    home view

--- spa/src/views/Home.vue
+++ spa/src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="home">
-    <div>{{$store.state.username}}</div>
-    <div>{{$store.state.role}}</div>
+    <table class="usrStateTable">
+      <tr>
+        <th>Logged in as:</th>
+        <td>{{$store.state.username}}</td>
+      </tr>
+      <tr>
+        <th>User Role:</th>
+        <td>{{$store.state.role}}</td>
+      </tr>
+    </table>
     
+    <img src="@/assets/logo_tools.png" class="opensslLogo" />
+
   </div>
 </template>
 
@@ -17,3 +27,28 @@ export default {
   }
 }
 </script>
+
+
+<style scoped>
+.opensslLogo{
+  margin: 18vh auto auto auto;
+  width:22%;
+  margin-left: 2%;
+  min-width: 300px;
+  max-width: 800px;
+
+}
+.usrStateTable{
+  position: fixed;
+  left:10px;
+  bottom: 10px;
+  font-size:13px;
+  text-align: left;
+}
+.usrStateTable th, td{
+  text-align: left;
+  border: 1px solid #ccc;
+  padding:2px 10px 2px 10px;
+}
+
+</style>
